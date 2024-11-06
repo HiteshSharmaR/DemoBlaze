6800ad392bdf85e57fcd3fadf781449d0f1becb7
Hitesh <honey.hitesh13@gmail.com>
2024-11-06 14:18:25 +0530

    Second attempt

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -4,4 +4,13 @@ public class HomePage {
     //public static By productName = By.xpath("//div[@id='tbodyid']//a[contains(text(), 'Samsung galaxy s6')]");
     public static By productName = By.xpath("//div[@class='card h-100']//a[contains(text(),'Samsung galaxy s6')]");
     public static By addToCartButton = By.xpath("//div[@class='col-sm-12 col-md-6 col-lg-6']//a");
+    public static By cart = By.xpath("//a[@id='cartur']");
+    public static By placeOrderButton = By.xpath("//div[@class='col-lg-1']//button");
+    public static By textFieldName = By.xpath("//input[@id='name']");
+    public static By textFieldCountry = By.xpath("//input[@id='country']");
+    public static By textFieldCity = By.xpath("//input[@id='city']");
+    public static By textFieldCreditCard = By.xpath("//input[@id='card']");
+    public static By textFieldMonth = By.xpath("//input[@id='month']");
+    public static By textFieldYear = By.xpath("//input[@id='year']");
+    public static By purchaseButton = By.xpath("//button[contains(text(),'Purchase')]");
 }
--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -1,6 +1,5 @@
-import org.openqa.selenium.By;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
@@ -15,5 +14,38 @@ public class HomePageTest extends BaseClass{
         driver.findElement(HomePage.productName).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(HomePage.addToCartButton));
         driver.findElement(HomePage.addToCartButton).click();
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = driver.switchTo().alert();
+        String alertMessage = alert.getText();
+        System.out.print(alertMessage);
+        alert.accept();
+        driver.findElement(HomePage.cart).click();
+//        wait.until(ExpectedConditions.elementToBeClickable(HomePage.placeOrderButton));
+//        driver.findElement(HomePage.placeOrderButton).click();
+        WebElement openModalButton = driver.findElement(HomePage.placeOrderButton);
+        openModalButton.click();
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+        wait.until(ExpectedConditions.elementToBeClickable(HomePage.textFieldName));
+        WebElement textField1 = driver.findElement(HomePage.textFieldName);
+        textField1.sendKeys("Test");
+        WebElement textField2 = driver.findElement(HomePage.textFieldCountry);
+        textField2.sendKeys("India");
+        WebElement textField3 = driver.findElement(HomePage.textFieldCity);
+        textField3.sendKeys("Chandigarh");
+        WebElement textField4 = driver.findElement(HomePage.textFieldYear);
+        textField4.sendKeys("2024");
+        WebElement textField5 = driver.findElement(HomePage.textFieldCreditCard);
+        textField5.sendKeys("1123456789");
+        WebElement purchaseButton = driver.findElement(HomePage.purchaseButton);
+        purchaseButton.click();
     }
 }
